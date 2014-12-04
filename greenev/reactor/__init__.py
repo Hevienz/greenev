5358346c00d0ac5add22ba2edeec8ebc824f79ff
Hevienz <hevienz@qq.com>
2014-12-04 12:58:46 +0800

    add pollreactor

--- greenev/reactor/__init__.py
+++ greenev/reactor/__init__.py
@@ -2,5 +2,7 @@ import select
 
 if 'epoll' in select.__dict__:
     from epollreactor import EpollReactor as Reactor
+elif 'poll' in select.__dict__:
+    from pollreactor import PollReactor as Reactor
 else:
     from selectreactor import SelectReactor as Reactor
