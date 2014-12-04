66e7c6f18c2d9f2c86eb263ff62f8bae3ea158bd
Hevienz <hevienz@qq.com>
2014-12-04 13:32:50 +0800

    add kqueuereactor

--- greenev/reactor/__init__.py
+++ greenev/reactor/__init__.py
@@ -1,8 +1,14 @@
 import select
+import sys
 
 if 'epoll' in select.__dict__:
     from epollreactor import EpollReactor as Reactor
+elif 'kqueue' in select.__dict__:
+    from kqueuereactor import KqueueReactor as Reactor
 elif 'poll' in select.__dict__:
     from pollreactor import PollReactor as Reactor
-else:
+elif 'select' in select.__dict__:
     from selectreactor import SelectReactor as Reactor
+else:
+    print "There is no reactor can be used." 
+    sys.exit(1)
