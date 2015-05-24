8a018b24c0c6dea367b347dfc75064b8c9e78e0e
Hevienz <hevienz@qq.com>
2015-05-24 16:01:44 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="greenev",
-    version="0.1.3",
+    version="0.1.4",
     description="greenev is a Python networking service framework that bseed on greenlet's coroutine, it is event driven and use non-blocking socket model. It makes writing synchronous code gain the advantage of asynchronous execution.",
     author="Hevienz",
     url="https://github.com/Hevienz/greenev",
